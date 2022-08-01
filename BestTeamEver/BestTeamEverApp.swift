b69d7a370302e26bb3915447ad4148f0103aa6c0
tanya_bond <bondarenkotatiana96@gmail.com>
2022-08-01 09:41:56 -0700

    Initial commit

---
+++ BestTeamEver/BestTeamEverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestTeamEverApp.swift
+//  BestTeamEver
+//
+//  Created by Tatiana Bondarenko on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BestTeamEverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
